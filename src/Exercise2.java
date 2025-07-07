080dd1e244de05d98d1b362a978d6a82c9355004
Andrés <andrescalvo01@gmail.com>
2025-07-07 10:36:12 +0200

    commit 1

---
+++ src/Exercise2.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Exercise2 {
+    public static void main(String[] args){
+
+        List<Integer> numberList= new ArrayList<>();
+        numberList.add(10);
+        numberList.add(20);
+        numberList.add(30);
+        numberList.add(40);
+        numberList.add(50);
+
+        System.out.println("List with the original order");
+        System.out.println(numberList);
+
+        List<Integer>reversedNumberList= new ArrayList<>();
+        ListIterator<Integer>iterator= numberList.listIterator(numberList.size());
+
+        while(iterator.hasPrevious()){
+            reversedNumberList.add(iterator.previous());
+        }
+        System.out.println("List with a reverse order");
+        System.out.println(reversedNumberList);
+
+
+
+    }
+}
